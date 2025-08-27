1f1e0affb33f13847ee2aee3f450289a2d941264
emrejuhani <emre.acarca@gmail.com>
2025-08-27 10:26:37 +0300

    yhteenlaskut

---
+++ Yhteenlasku/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Anna ensimmäinen luku: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Anna toinen luku: ");
+        int b = int.Parse(Console.ReadLine());
+
+        int summa = a + b;
+
+        Console.WriteLine($"{a} + {b} = {summa}");
+    }
+}
