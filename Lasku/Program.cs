23dba020bc73ec21d91c675358f10262ffd8aa6c
TohtoriTampere <aaronvristian@gmail.com>
2025-08-27 12:09:11 +0300

    Update Program.cs

--- Lasku/Program.cs
+++ Lasku/Program.cs
@@ -1,32 +1,16 @@
-using system;
+using System;
 
-﻿class Program
+class Program
 {
     static void Main()
     {
-        Console.Write("Annas luku jaettavaksi ");
-        string? input1 = Console.ReadLine();
+        Console.Write("Anna jaettava luku: ");
+        double jaettava = double.Parse(Console.ReadLine());
 
-        Console.Write("Annas jakaja ");
-        string? input2 = Console.ReadLine();
+        Console.Write("Anna jakaja: ");
+        double jakaja = double.Parse(Console.ReadLine());
 
-        if (double.TryParse(input1, out double jaettava) && double.TryParse(input2, out double jakaja))
-        {
-            if (jakaja == 0)
-            {
-                Console.WriteLine("Nollalla ei voi jakaa");
-            }
-            else
-            {
-                double tulos = jaettava / jakaja;
-                Console.WriteLine($"Tulos: {jaettava} / {jakaja} = {tulos}");
-            }
-        }
-        else
-        {
-            Console.WriteLine("VIRHE!");
-        }
-
-        Console.ReadLine();
+        double tulos = jaettava / jakaja;
+        Console.WriteLine($"{jaettava} / {jakaja} = {tulos}");
     }
 }
